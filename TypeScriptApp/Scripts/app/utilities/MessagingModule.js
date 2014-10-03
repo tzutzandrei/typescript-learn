4c652d30e003b901d45b08d0eb4447c97c6534a4
Tut <andrei.tzutz@synergogroup.net>
2014-10-03 14:28:43 +0300

    changes

---
+++ TypeScriptApp/Scripts/app/utilities/MessagingModule.js
@@ -0,0 +1,20 @@
+﻿var Messaging;
+(function (Messaging) {
+    var Message = (function () {
+        function Message() {
+        }
+        return Message;
+    })();
+
+    function subscribe(callback, subscriberName) {
+    }
+    Messaging.subscribe = subscribe;
+    ;
+
+    function sendMessage(subscriberName, message) {
+    }
+    Messaging.sendMessage = sendMessage;
+    ;
+})(Messaging || (Messaging = {}));
+;
+//# sourceMappingURL=MessagingModule.js.map
---
+++ TypeScriptApp/Scripts/app/utilities/MessagingModule.ts
@@ -0,0 +1,15 @@
+﻿module Messaging {
+    
+    class Message {
+
+    }
+
+
+    export function subscribe(callback: (message: string, parameter: any) => void, subscriberName: string) {
+
+    };
+
+    export function sendMessage(subscriberName: string, message: string) {
+
+    };
+}; 
